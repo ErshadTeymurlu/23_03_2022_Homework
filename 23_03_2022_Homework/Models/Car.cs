19475d00c010f6f4d888036e31b5f2414dcf6b5d
Ershad Teymurlu <7ahgfnr@code.edu.az>
2022-03-19 16:05:57 +0400

    Skeleton

---
+++ 23_03_2022_Homework/Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_03_2022_Homework.Models
+{
+    internal class Car : Motored_Vehicles
+    {
+        public bool isOn;
+        public Car(string model, string make , string year) : base(model, make, year)
+        {
+
+        }
+        public void TurnOn()
+        {
+
+        }
+        public void TurnOff()
+        {
+
+        }
+        public void Go()
+        {
+
+        }
+        public override void GetDetailedInfo()
+        {
+
+        }
+
+        
+    }
+}
---
+++ 23_03_2022_Homework/Models/Motored_Vehicles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_03_2022_Homework.Models
+{
+    internal class Motored_Vehicles
+    {
+        public string model;
+        public string make;
+        public string productionYear;
+        public string motorSize;
+        public string fuelTankSize;
+        public string fuelAmount;
+        public string fuelUsedPerKm;
+        public string color;
+        public string transmission;
+        public string horsePower;
+        public string passangerCount;
+        public Motored_Vehicles()
+        {
+
+        }
+        public Motored_Vehicles(string model, string make, string productionYear)
+        {
+            this.model = model;
+            this.make = make;
+            this.productionYear = productionYear;
+        }
+        public void GetInfo()
+        {
+
+        }
+        public virtual void GetDetailedInfo()
+        {
+
+        }
+    }
+}
